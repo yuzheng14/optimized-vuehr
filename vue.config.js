c7e2db4563de6cf074a622168e32b497af337d3b
yuzheng14 <422450117@qq.com>
2023-04-23 22:05:41 +0800

    style(vue.config.js): 格式化

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,34 @@
-let proxyObj = {};
-const CompressionPlugin = require("compression-webpack-plugin");
+let proxyObj = {}
+const CompressionPlugin = require('compression-webpack-plugin')
 proxyObj['/ws'] = {
-    ws: true,
-    target: "ws://www.yuzheng14.com:8081"
-};
+  ws: true,
+  target: 'ws://www.yuzheng14.com:8081',
+}
 proxyObj['/'] = {
-    ws: false,
-    target: 'http://www.yuzheng14.com:8081',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/': ''
-    }
+  ws: false,
+  target: 'http://www.yuzheng14.com:8081',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/': '',
+  },
 }
 module.exports = {
-    devServer: {
-        host: 'localhost',
-        port: 8080,
-        proxy: proxyObj
-    },
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-            return {
-                plugins: [
-                    new CompressionPlugin({
-                        test: /\.js$|\.html$|\.css/,
-                        threshold: 1024,
-                        deleteOriginalAssets: false
-                    })
-                ]
-            }
-        }
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: proxyObj,
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css/,
+            threshold: 1024,
+            deleteOriginalAssets: false,
+          }),
+        ],
+      }
     }
+  },
 }
